0d34ce534c7b636b9b9cd89345c47d93256e3dda
mcenkbatman <mcenkbatman@gmail.com>
2020-12-05 15:41:55 +0300

    [docs] add mongodb connection string security comment

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const MONGO = {
+  // I know including connection strings in the public code is dumb but this connection string is for a free cluster and it will expire in 7 days
   URI: process.env.MONGO_CONNECTION_URI || 'mongodb+srv://cf-test:JjxEX3W0vgQwNiyx@cluster0.bwu4j.mongodb.net/test?retryWrites=true&w=majority',
   MAX_TIME_FOR_QUERY_AS_MS: parseInt(process.env.MONGO_MAX_TIME_FOR_QUERY_AS_MS || 5000),
   DEBUG: process.env.MONGO_DEBUG === 'TRUE',
